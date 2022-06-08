4fd60d605e2dbea591b6b9c99ccb2874976303a2
neetharmaliackal <neethamaliackal@gmail.com>
2022-06-08 08:25:25 +0530

    localstorageinitial setup

--- src/app/json-form/json-form.component.ts
+++ src/app/json-form/json-form.component.ts
@@ -41,6 +41,8 @@ export interface fvalidators {
   styleUrls: ['./json-form.component.css']
 })
 export class JsonFormComponent implements OnInit {
+  // user:any = {};
+  user:any = {};
   @Input() jsonFormData: any;
   public myForm: FormGroup = this.fb.group({});
   public formData!: Surveycontroller[];
@@ -53,9 +55,8 @@ export class JsonFormComponent implements OnInit {
     this.personalSurveyService.getPersonalData().
       subscribe((response) => {
         this.formData = response.surveycontrollers;
-        console.log("jsonform", response);
        this.myForm= this.toFormGroup(this.formData);
-        console.log("jsonform", response);
+    
       })
      
     // this.myForm = this.fb.group({
@@ -66,77 +67,42 @@ export class JsonFormComponent implements OnInit {
 
 
   toFormGroup(questions: Surveycontroller[] ) {
+  
     let group: any = {};
 
     questions.forEach(question => {
+    
       group[question.formcontrolName] = question?.validators?.required ? new FormControl(question.value || '', Validators.required)
                                               : new FormControl(question.value || '');
     });
+    console.log(group);
     return new FormGroup(group);
   }
 
 
-  // ngOnChanges(changes: SimpleChanges) {
-  //   if (!changes["jsonFormData"].firstChange) {
-  //     this.createForm(this.jsonFormData.controls);
-  //   }
-  // }
-  // createForm(controls: Surveycontroller[]) {
-  //   for (const control of controls) {
-  //     const validatorsToAdd = [];
-  //     for (const [key, value] of Object.entries(control.validators)) {
-  //       switch (key) {
-  //         case 'min':
-  //           validatorsToAdd.push(Validators.min(value));
-  //           break;
-  //         case 'max':
-  //           validatorsToAdd.push(Validators.max(value));
-  //           break;
-  //         case 'required':
-  //           if (value) {
-  //             validatorsToAdd.push(Validators.required);
-  //           }
-  //           break;
-  //         case 'requiredTrue':
-  //           if (value) {
-  //             validatorsToAdd.push(Validators.requiredTrue);
-  //           }
-  //           break;
-  //         case 'email':
-  //           if (value) {
-  //             validatorsToAdd.push(Validators.email);
-  //           }
-  //           break;
-  //         case 'minLength':
-  //           validatorsToAdd.push(Validators.minLength(value));
-  //           break;
-  //         case 'maxLength':
-  //           validatorsToAdd.push(Validators.maxLength(value));
-  //           break;
-  //         case 'pattern':
-  //           validatorsToAdd.push(Validators.pattern(value));
-  //           break;
-  //         case 'nullValidator':
-  //           if (value) {
-  //             validatorsToAdd.push(Validators.nullValidator);
-  //           }
-  //           break;
-  //         default:
-  //           break;
-  //       }
-  //     }
-  //     this.myForm.addControl(
-  //       control.text,
-  //       this.fb.control(control.value, validatorsToAdd)
-  //     );
-  //   }
-  // }
-  // onSubmit() {
-  //   console.log('Form valid: ', this.myForm.valid);
-  //   console.log('Form values: ', this.myForm.value);
-  // }
+  
   saveForm(){
    console.log('Form valid: ', this.myForm.valid);
   console.log('Form values: ', this.myForm.value);
+  this.user=Object.assign(this.user, this.myForm.value);
+
+  localStorage.setItem('Users',JSON.stringify(this.user));
+  // this.addUser(this.user);
+  // this.myForm.reset();
   }
+
+  // addUser(user: any){
+  //   let users=[];
+  //   if(localStorage.getItem('Users')){
+  //     users= JSON.parse(localStorage.getItem('Users') || '{}');
+  //   //  users= JSON.parse(localStorage.getItem('Users') || '{}');
+  //     // users=JSON.parse(localStorage.getItem('Users'))
+  //     users=[user, ...users];
+  //   }
+  //   else{
+  //     users=[user];
+  //   }
+  //   localStorage.setItem('Users',JSON.stringify(users));
+  // }
+
 }
